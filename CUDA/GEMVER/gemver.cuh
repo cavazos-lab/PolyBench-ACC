a065811c3756314b8e8d289c862b3bf5debeabc8
sgrauerg@udel.edu <sgrauerg@udel.edu>
2013-08-28 23:55:11 -0700

    Updated cuda opencl; now based on Polybench 3.2

---
+++ CUDA/GEMVER/gemver.cuh
@@ -0,0 +1,63 @@
+/**
+ * gemver.cuh: This file is part of the PolyBench/GPU 1.0 test suite.
+ *
+ *
+ * Contact: Scott Grauer-Gray <sgrauerg@gmail.com>
+ * Will Killian <killian@udel.edu>
+ * Louis-Noel Pouchet <pouchet@cse.ohio-state.edu>
+ * Web address: http://www.cse.ohio-state.edu/~pouchet/software/polybench/GPU
+ */
+
+#ifndef GEMVER_CUH
+# define GEMVER_CUH
+
+/* Default to STANDARD_DATASET. */
+# if !defined(MINI_DATASET) && !defined(SMALL_DATASET) && !defined(LARGE_DATASET) && !defined(EXTRALARGE_DATASET)
+#  define STANDARD_DATASET
+# endif
+
+/* Do not define anything if the user manually defines the size. */
+# if !defined(N)
+/* Define the possible dataset sizes. */
+#  ifdef MINI_DATASET
+#define N 4096
+#  endif
+
+#  ifdef SMALL_DATASET
+#define N 4096
+#  endif
+
+#  ifdef STANDARD_DATASET /* Default if unspecified. */
+#define N 4096
+#  endif
+
+#  ifdef LARGE_DATASET
+#define N 4096
+#  endif
+
+#  ifdef EXTRALARGE_DATASET
+#define N 4096
+#  endif
+# endif /* !N */
+
+# define _PB_N POLYBENCH_LOOP_BOUND(N,n)
+
+# ifndef DATA_TYPE
+#  define DATA_TYPE float
+#  define DATA_PRINTF_MODIFIER "%0.2lf "
+# endif
+
+/* Thread block dimensions for kernel 1*/
+#define DIM_THREAD_BLOCK_KERNEL_1_X 32
+#define DIM_THREAD_BLOCK_KERNEL_1_Y 8
+
+/* Thread block dimensions for kernel 2*/
+#define DIM_THREAD_BLOCK_KERNEL_2_X 256
+#define DIM_THREAD_BLOCK_KERNEL_2_Y 1
+
+/* Thread block dimensions for kernel 3*/
+#define DIM_THREAD_BLOCK_KERNEL_3_X 256
+#define DIM_THREAD_BLOCK_KERNEL_3_Y 1
+
+
+#endif /* !TWODCONV*/
---
+++ CUDA/MVT/mvt.cuh
@@ -0,0 +1,55 @@
+/**
+ * mvt.cuh: This file is part of the PolyBench/GPU 1.0 test suite.
+ *
+ *
+ * Contact: Scott Grauer-Gray <sgrauerg@gmail.com>
+ * Will Killian <killian@udel.edu>
+ * Louis-Noel Pouchet <pouchet@cse.ohio-state.edu>
+ * Web address: http://www.cse.ohio-state.edu/~pouchet/software/polybench/GPU
+ */
+
+#ifndef MVT_CUH
+# define MVT_CUH
+
+/* Default to STANDARD_DATASET. */
+# if !defined(MINI_DATASET) && !defined(SMALL_DATASET) && !defined(LARGE_DATASET) && !defined(EXTRALARGE_DATASET)
+#  define STANDARD_DATASET
+# endif
+
+/* Do not define anything if the user manually defines the size. */
+# if !defined(N)
+/* Define the possible dataset sizes. */
+#  ifdef MINI_DATASET
+#define N 4096
+#  endif
+
+#  ifdef SMALL_DATASET
+#define N 4096
+#  endif
+
+#  ifdef STANDARD_DATASET /* Default if unspecified. */
+#define N 4096
+#  endif
+
+#  ifdef LARGE_DATASET
+#define N 4096
+#  endif
+
+#  ifdef EXTRALARGE_DATASET
+#define N 4096
+#  endif
+# endif /* !N */
+
+# define _PB_N POLYBENCH_LOOP_BOUND(N,n)
+
+# ifndef DATA_TYPE
+#  define DATA_TYPE float
+#  define DATA_PRINTF_MODIFIER "%0.2lf "
+# endif
+
+/* Thread block dimensions */
+#define DIM_THREAD_BLOCK_X 32
+#define DIM_THREAD_BLOCK_Y 8
+
+
+#endif /* !MVT*/
