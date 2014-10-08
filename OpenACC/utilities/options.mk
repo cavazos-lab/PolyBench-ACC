5b55be696b9fe966a681d2724cd3207678d8564e
Tristan Vanderbruggen <vanderbruggentristan@gmail.com>
2014-10-08 14:13:04 -0400

    Fix problem when device is not recognized by libOpenACC

--- OpenACC/utilities/options.mk
+++ OpenACC/utilities/options.mk
@@ -8,9 +8,9 @@ ACC = roseacc
 # Accelerator Compiler flags
 ACCFLAGS=--roseacc:desc_format=static_data --roseacc:compile=false
 
-ACC_INC_PATH=`openacc --incpath`
-ACC_LIB_PATH=`openacc --libpath`
-ACC_LIBS=`openacc --libs`
+ACC_INC_PATH=`openacc --incpath | tail -n1`
+ACC_LIB_PATH=`openacc --libpath | tail -n1`
+ACC_LIBS=`openacc --libs | tail -n1`
 
 # COMPILER OPTIONS -- HOST
 ########################################
